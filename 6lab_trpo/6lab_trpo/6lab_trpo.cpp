ac68371a22fd5f4253d6b36fdd677c8215450091
Екатерина <@DESKTOP-F6U1U50>
2026-05-22 13:35:23 +0400

    Реализация классов + проверка работы CopySyntaxTree

--- 6lab_trpo/6lab_trpo/6lab_trpo.cpp
+++ 6lab_trpo/6lab_trpo/6lab_trpo.cpp
@@ -1,10 +1,12 @@
 ﻿#include <iostream>
+#include <cassert>
 
 struct Transformer;
 struct Number;
 struct BinaryOperation;
 struct FunctionCall;
 struct Variable;
+
 struct Expression
 {
 	virtual ~Expression() {}
@@ -100,3 +102,77 @@ struct CopySyntaxTree : Transformer
 		return new Variable(var->name());
 	}
 };
+
+//Number: число (хранит значение)
+Number::Number(double value) : value_(value) {}
+double Number::value() const { return value_; }
+double Number::evaluate() const { return value_; }
+Expression* Number::transform(Transformer* tr) const {
+	return tr->transformNumber(this);
+}
+//BinaryOperation: бинарная операция (+, -, *, / )
+BinaryOperation::BinaryOperation(Expression const* left, int op, Expression const* right)
+	: left_(left), op_(op), right_(right) {
+}
+BinaryOperation::~BinaryOperation() { delete left_; delete right_; }
+double BinaryOperation::evaluate() const {
+	double l = left_->evaluate();
+	double r = right_->evaluate();
+	switch (op_) {
+	case BinaryOperation::PLUS: return l + r;
+	case BinaryOperation::MINUS: return l - r;
+	case BinaryOperation::DIV: return l / r;
+	case BinaryOperation::MUL: return l * r;
+	}
+	return 0.0;
+}
+Expression* BinaryOperation::transform(Transformer* tr) const {
+	return tr->transformBinaryOperation(this);
+}
+Expression const* BinaryOperation::left() const { return left_; }
+Expression const* BinaryOperation::right() const { return right_; }
+int BinaryOperation::operation() const { return op_; }
+
+//FunctionCall: вызов функции (sqrt или abs)
+FunctionCall::FunctionCall(std::string const& name, Expression const* arg)
+	: name_(name), arg_(arg) {
+}
+FunctionCall::~FunctionCall() { delete arg_; }
+double FunctionCall::evaluate() const {
+	double a = arg_->evaluate();
+	if (name_ == "sqrt") return sqrt(a);
+	if (name_ == "abs") return fabs(a);
+	return 0.0;
+}
+Expression* FunctionCall::transform(Transformer* tr) const {
+	return tr->transformFunctionCall(this);
+}
+std::string const& FunctionCall::name() const { return name_; }
+Expression const* FunctionCall::arg() const { return arg_; }
+
+//Variable: переменная (значение не задаётся, evaluate() возвращает 0)
+Variable::Variable(std::string const name) : name_(name) {}
+std::string const& Variable::name() const { return name_; }
+double Variable::evaluate() const { return 0.0; }
+Expression* Variable::transform(Transformer* tr) const {
+	return tr->transformVariable(this);
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Number* n32 = new Number(32.0);
+	Number* n16 = new Number(16.0);
+	BinaryOperation* minus = new BinaryOperation(n32, BinaryOperation::MINUS, n16);
+	FunctionCall* callSqrt = new FunctionCall("sqrt", minus);
+	Variable* var = new Variable("var");
+	BinaryOperation* mult = new BinaryOperation(var, BinaryOperation::MUL,
+		callSqrt);
+	FunctionCall* callAbs = new FunctionCall("abs", mult);
+	CopySyntaxTree CST;
+	Expression* newExpr = callAbs->transform(&CST);
+
+	std::cout << "Оригинал: " << callAbs->evaluate() << std::endl;
+	std::cout << "Копия: " << newExpr->evaluate() << std::endl;
+}
